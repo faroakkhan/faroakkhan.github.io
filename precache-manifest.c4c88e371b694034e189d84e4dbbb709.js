4b9418d04260023171a1c018036b44ef6381914a
AungMyoOo <dev.aungmyooo2015@gmail.com>
2020-09-26 01:33:07 +0630

    Updates

---
+++ precache-manifest.c4c88e371b694034e189d84e4dbbb709.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21546e5bd0d826fe7fbe6fb7f7a8f089",
+    "url": "/faroakkhan/index.html"
+  },
+  {
+    "revision": "046394294b9973c3d15d",
+    "url": "/faroakkhan/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9f11f774ccfa087d8a9",
+    "url": "/faroakkhan/static/js/2.203c5731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faroakkhan/static/js/2.203c5731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046394294b9973c3d15d",
+    "url": "/faroakkhan/static/js/main.ae89c78c.chunk.js"
+  },
+  {
+    "revision": "34564e016315883c48d4",
+    "url": "/faroakkhan/static/js/runtime-main.2d7da1e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/faroakkhan/static/media/logo.5d5d9eef.svg"
+  }
+]);
